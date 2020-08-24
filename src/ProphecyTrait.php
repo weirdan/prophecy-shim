9cc861f15b1cd5e5e254a2b18b4b8805dd4b2009
Bruce Weirdan <weirdan@gmail.com>
2020-08-24 16:35:06 +0300

    Implementation for older PHPUnit versions

---
+++ src/ProphecyTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Prophecy\PhpUnit;
+
+/**
+ * Empty trait that you can import into tests used with PHPUnit <9.1 to provide
+ * forward compatibility with prophecy-phpunit
+ *
+ * Actual implementation (prophesize() et al) is provided by
+ * PHPUnit\Framework\TestCase itself.
+ */
+trait ProphecyTrait
+{
+}
